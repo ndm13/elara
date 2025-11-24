402bb0b23f213dc7ae6146d64354cfdb1ac4c546
ndm13 <ndm0113@gmail.com>
2025-11-23 22:10:30 -0500

    Add basic logging

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {AIDungeonAPI} from "./common/AIDungeonAPI.ts";
-import { Client, extendContext } from "seyfert";
+import { Client, extendContext, createMiddleware } from "seyfert";
 
 const api = await AIDungeonAPI.guest();
 const context = extendContext((_interaction) => ({ api }));
@@ -9,7 +9,26 @@ declare module 'seyfert' {
     interface ExtendContext extends ReturnType<typeof context> {}
 }
 
-const client = new Client({ context });
+const client = new Client({ context, globalMiddlewares: ["logger"] });
+
+client.setServices({
+    middlewares: {
+        logger: createMiddleware<void>(
+            (middle) => {
+                if (middle.context.isChat())
+                    middle.context.client.logger.info(`@${middle.context.author.username}: /${middle.context.resolver.fullCommandName}`);
+                if (middle.context.isComponent())
+                    middle.context.client.logger.info(`@${middle.context.author.username}: [${middle.context.customId}]`);
+                if (middle.context.isMenu())
+                    middle.context.client.logger.info(`@${middle.context.author.username}: <${middle.context.customId}>`);
+                if (middle.context.isModal())
+                    middle.context.client.logger.info(`@${middle.context.author.username}: (${middle.context.customId})`);
+                middle.next();
+            }
+        )
+    }
+})
+
 client
     .start()
     .then(() => client.uploadCommands({ cachePath: './commands.json' }));
